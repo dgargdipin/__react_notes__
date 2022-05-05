ce41f7ec02b2503574e835190e447120fb6fe814
Dipin Garg <dgargdipin@gmail.com>
2022-05-06 00:39:36 +0530

    update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,33 @@
 
 console.log("App.js is running");
 var template = React.createElement(
-  "p",
-  null,
-  "This is jsx from app.js changesss"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "This is jsx from app.js changesss"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 console.log("App.js is running")
-var template = <p>This is jsx from app.js changesss</p>;
-var appRoot=document.getElementById('app');
-ReactDOM.render(template,appRoot)
+var template = (
+    <div>
+        <h1>This is jsx from app.js changesss</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>
+                Item One
+            </li>
+            <li>
+                Item two
+            </li>
+        </ol>
+    </div>
+);
+
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot)
