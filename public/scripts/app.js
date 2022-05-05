3d289fde4a10a66deee2de79ed328474621e5ac1
Dipin Garg <dgargdipin@gmail.com>
2022-05-06 00:28:59 +0530

    added babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,10 @@
-console.log("App.js is running")
-var template = <p>This is jsx from app.js</p>;
-var appRoot=document.getElementById('app');
-ReactDOM.render(template,appRoot)
+"use strict";
+
+console.log("App.js is running");
+var template = React.createElement(
+  "p",
+  null,
+  "This is jsx from app.js changesss"
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
