baaa303a0043d8e7576fae8ee4d1e7d71a757d80
Dipin Garg <dgargdipin@gmail.com>
2022-05-06 18:21:28 +0530

    es6 classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,12 +11,54 @@ class Person{
         return `${this.name} is ${this.age} years old.`;
     }
 }
+class Student extends Person{
+    constructor(name,age,major){
+        super(name,age);
+        this.major=major;
+    }
+    hasMajor(){
+        return !!this.major
+    }
+    getDescription() {
+        if(!this.hasMajor())return super.getDescription();
+        return `${this.name} is ${this.age} years old is in ${this.major} branch.`;
 
-const me=new Person('Dipin',20);
-const other=new Person();
+    }
+
+}
+
+
+class Traveller extends Person{
+    constructor(name,age,homeLocation){
+        super(name,age);
+        this.homeLocation=homeLocation;
+    }
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+    getGreeting(){
+        let greeting=super.getGreeting();
+        if(this.hasHomeLocation()){
+            greeting+=`I come from ${this.homeLocation}.`
+        }
+        return greeting;
+
+    }
+}
+const me=new Student('Dipin',20,'CSE');
+const other=new Student();
+const traveller1=new Traveller('t1',20,'Delhi');
+const traveller2=new Traveller('t2',20);
+
 console.log(me,other);
-console.log(me.getGreeting());
-console.log(me.getDescription());
-console.log(other.getGreeting());
-console.log(other.getDescription());
+console.log(me.hasMajor())
+console.log(me.getDescription())
+console.log(other.hasMajor())
+console.log(other.getDescription())
+console.log(traveller1.getGreeting())
+console.log(traveller2.getGreeting())
+// console.log(me.getGreeting());
+// console.log(me.getDescription());
+// console.log(other.getGreeting());
+// console.log(other.getDescription());
 
