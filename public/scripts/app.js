7e0e4140be5daa35bf80c1a826957006dfef6eb0
Dipin Garg <dgargdipin@gmail.com>
2022-05-06 15:23:37 +0530

    arrow fn

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,76 @@
-"use strict";
+'use strict';
 
-var add = function add(a, b) {
-    console.log(arguments);
-    return a + b;
+console.log("App.js is running");
+var appInfo = {
+    heading: 'Hi',
+    subtitle: 'subssssss',
+    options: ['One', 'Two']
+};
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appInfo.heading
+    ),
+    appInfo.subtitle && React.createElement(
+        'p',
+        null,
+        appInfo.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        appInfo.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+var user = {
+    name: 'Dip in',
+    age: 26,
+    location: "India"
 };
 
-console.log(add(55, 1, 1001));
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'location: ',
+            location
+        );
+    }
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -4,3 +4,85 @@ const add=function(a,b){
 };
 
 console.log(add(55,1,1001));
+
+const addArrow = (a, b)=> {
+    // console.log(arguments)
+    return a + b;
+};
+
+
+const user={
+    name:'Andrew',
+    cities:['Delhi','Noida'],
+    printPlacesLived:function(){// cannot use arrow fn here
+        console.log(this.name)
+        console.log(this.cities)
+        const that=this;
+        this.cities.forEach(function(city){
+            console.log(that.name+' has lived in '+ city);
+
+        })
+        this.cities.forEach((city)=>{
+            console.log(this.name+' has lived in '+ city);
+
+        })
+
+    },
+    printPlacesLivedConcise(){// same as above
+        console.log(this.name)
+        console.log(this.cities)
+        const that=this;
+        this.cities.forEach(function(city){
+            console.log(that.name+' has lived in '+ city);
+
+        })
+        this.cities.forEach((city)=>{
+            console.log(this.name+' has lived in '+ city);
+
+        })
+
+    }
+}
+
+user.printPlacesLived()
+user.printPlacesLivedConcise()
+
+
+const user2={
+    name: 'Andrew',
+        cities: ['Delhi', 'Noida'],
+            printPlacesLived: function() {// cannot use arrow fn here
+                console.log(this.name)
+                console.log(this.cities)
+                const that = this;
+                this.cities.forEach(function (city) {
+                    console.log(that.name + ' has lived in ' + city);
+
+                })
+                this.cities.forEach((city) => {
+                    console.log(this.name + ' has lived in ' + city);
+
+                })
+
+            },
+    getCities(){// same as above
+        return this.cities.map((city)=> this.name + ' has lived in ' + city)
+        
+
+    }
+}
+
+console.log(user2.getCities());
+
+
+
+
+const multiplier={
+    numbers:[1,2,3,4],
+    multiplyBy:3,
+    multiply(){
+        return this.numbers.map((item)=>item*this.multiplyBy);
+    }
+}
+
+console.log(multiplier.multiply())
