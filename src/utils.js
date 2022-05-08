a77d1065d5c7e65f34383a7f3689f48e282d82db
Dipin Garg <dgargdipin@gmail.com>
2022-05-08 16:16:27 +0530

    default export

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,5 @@ console.log('utils.js is running')
 
 export const square=(x)=>x*x;
 export const add=(a,b)=>a+b;
-const subtract=(a,b)=>a-b;
 export const newfn=()=>'abc'
-export default subtract
+export default (a, b) => a - b;
