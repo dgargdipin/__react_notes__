a0b72f2ea2490c73f0571b82e70e0ca49bf92db9
Dipin Garg <dgargdipin@gmail.com>
2022-05-08 17:10:44 +0530

    webpack babel

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const template=React.createElement('p',{},'TESTING')
+const template=<div>ABC</div>
+
 ReactDOM.render(template,document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ const path=require('path')
 
 module.exports={
     entry:"./src/app.js",
-    mode: 'development',
+    mode: 'production',
     output:{
         path: path.join(__dirname,'public'),
         filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            loader:'babel-loader',
+            test:/\.js$/,
+            exclude:/node_modules/
+        }]
     }
 }
